bc6271e35e28fcc2bd2d0d4a9b20f2a21da92b73
Pito Salas <pitosalas@gmail.com>
2014-03-20 21:45:32 -0400

    getting ready for Ajax

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,11 +23,20 @@ class ItemsController < ApplicationController
 
   def search
     @categories = Category.all.map {|c| [c.title, c.id]}
+    @categories = [["any", 0]] + @categories
+    @items = nil
   end
 
   def do_search
-    binding.pry
-    @items = Item.where(title: params[:title], category_id: params[:category_id])
+    match_parm = params.fetch(:match_no_match)
+    all_any_parm = params.fetch(:all_any)
+    title_parm = params.fetch(:title)
+    cat_id_parm = params.fetch(:category_id)
+    @items = Item.do_search(
+          title: title_parm,
+          cat: cat_id_parm,
+          all_any: all_any_parm,
+          match_no: match_parm)
     render :index
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :comments
+
+  def self.do_search(title=nil, cat=nil, all_any=nil, match_no=nil)
+    self.all
+  end
 end
