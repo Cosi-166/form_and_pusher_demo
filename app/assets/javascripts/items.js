bc9f8f1beb72df0b517ff9fc1e85b2b6b94e59d6
pitosalas <pitosalas@gmail.com>
2016-04-06 09:37:36 -0400

    fixed ajax problem

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,4 +1,4 @@
-// Using pusher.com: When user is looking at any of the item views, 
+// Using pusher.com: When user is looking at any of the item views,
 // we will be listening to events on the comment_event channel ("comment_event").
 // When such an event comes in we receive item_id and count and
 // we surgically update the DOM
@@ -9,3 +9,11 @@ channel.bind('comment_event', function(data) {
 	var elt_id = 'comment-count-'+data.item_id;
 	document.getElementById(elt_id).innerHTML = data.count;
 });
+
+function submitFrmAjax(){
+   $.ajax({
+//Your ajax code here to submit from via AJAX. refer jquery site . I gave sample eg.
+         url : "/items/do_search",
+         data : $("form#search").serialize()
+    });
+}
